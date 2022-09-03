181ae50023280774a8bd88b3b1e2e869dd6b671d
NikKuh22 <nikuh1018@gmail.com>
2022-09-03 10:13:26 +0300

    Final

--- Test1/Model/MovieModel.swift
+++ Test1/Model/MovieModel.swift
@@ -3,15 +3,6 @@ import Foundation
 struct MovieModel: Hashable {
     let title: String
     let year: Int
-    
-    init() {
-        title = ""
-        year = 0
-    }
-    
-    init(title: String, year: Int) {
-        self.title = title
-        self.year = year
-    }
+
 }
 
